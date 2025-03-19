b1214e5d45053f55d97b8bc9d0ff44a4949569d1
IT 여행자 <66349648+hiparkwg@users.noreply.github.com>
2025-03-20 01:59:05 +0900

    Update localStorage.js

--- js/localStorage.js
+++ js/localStorage.js
@@ -1,58 +1,55 @@
 ( (frm)=>{
-    console.log("ready...")
-    let data=[];
-    let id="a1234";
+    let data=[] // 폼의 값이 실시간으로 저장될 변수
+    let id="a1234"; // 저장할 때 사용되는 키값
     load();
     frm.addEventListener("input", (ev)=>{
         store(frm);
     })
-  
-
     function store(frm){
         let formData = new FormData(frm);
+        let props;
         data = [];
-        for( let[key, value] of formData.entries()){
-            let tagType = frm.querySelector(`[name="${key}"]`).type;
-            console.log(key, tagType, value)
+        for( let[tagName, value] of formData.entries()){
+            let tagType = frm.querySelector(`[name="${tagName}"]`).type;
             props = {
-                'tagName' : key,
+                'tagName' : tagName,
                 'tagType' : tagType,
                 'value'   : value
             }
-            data.push(props)
-            
+            data.push(props);
         }
-        console.table(data)
-        localStorage.setItem(id, JSON.stringify(data))
+        localStorage.setItem(id, JSON.stringify(data));
     }
 
-    //window.onbeforeunload = store(document.frm)
+    // 새로고침하거나 다시 브라우저를 실행했을 때
     function load(){
-        let choiceType = ['radio','checkbox','select']
+        let choiceType = ["radio", "checkbox", "select"];
         let data = localStorage.getItem(id);
-        console.log('onload', data);
         if(data){
             let json = JSON.parse(data);
-            json.forEach(d =>{
+            json.forEach(d=>{
                 if(choiceType.includes(d.tagType)){
                     let list = document.querySelectorAll(`[name="${d.tagName}"]`);
                     list.forEach(target=>{
-                        if(target.value == d.value) target.checked=true;
+                        if(target.value == d.value) target.checked = true;
                     })
                 }else{
                     let tag = document.querySelector(`[name="${d.tagName}"]`);
                     tag.value = d.value;
                 }
+
+
             })
-        }else{
-            console.log("not found data.")
         }
     }
 
+    // 저장소 삭제
     let btnDelete = document.querySelector("#btnDelete");
     btnDelete.addEventListener("click", ()=>{
-        localStorage.removeItem(id)
-    })    
-
+        let yn = confirm("정말 삭제하시겠습니까? ");
+        if(yn){
+            localStorage.removeItem(id);
+        }
+    })
 
 })(document.frm)
